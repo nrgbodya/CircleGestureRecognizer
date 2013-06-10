84b3af66ebac062e9099c3f2c7a1ef33fc893c3f
Bogdan Weidmann <bweidmann@Administrators-iMac.local>
2013-06-10 13:41:19 +0200

    Initial Commit

---
+++ CircleGestureRecognizerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CircleGestureRecognizerDemo
+//
+//  Created by Bogdan Weidmann on 10.06.13.
+//  Copyright (c) 2013 nexiles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CircleGestureRecognizerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CircleGestureRecognizerDemo
+//
+//  Created by Bogdan Weidmann on 10.06.13.
+//  Copyright (c) 2013 nexiles. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
